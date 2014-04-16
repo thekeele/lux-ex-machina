81eb13ae4a88fe7eb0f99a51e339981a242752b3
Captain Underpants <mkeele@indiana.edu>
2014-04-16 00:00:08 -0400

    awesome icons, values behind sliders

--- public/javascripts/slider.js
+++ public/javascripts/slider.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-  // slider js
-  $( "#f_slider" ).slider();
-  $( "#s_slider" ).slider();
+  /* slider js */
+  
+  // aperture slider
+  $( "#f_slider" ).slider({ 
+    animate: "slow", // or "fast"
+    max: 22,
+    min: 1,
+    step: 0.5,
+    value: 3.5
+  });
+  // shutter speed slider
+  $( "#s_slider" ).slider({ 
+    animate: "slow", // or "fast"
+    max: 30,
+    min: 0.00025, // 1/4000
+    step: 1,
+    value: 1
+  });
   // end slider js
+
+  //$( "button#f_lock" ).click(fuction() {
+    //$( "f_slider" ).slider( "disable" );
+  //});
 }); // end document js
