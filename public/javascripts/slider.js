f5ed3614412fa573e318d4a67a5d0ec8e9b47081
Captain Underpants <mkeele@indiana.edu>
2014-04-15 22:02:55 -0400

    basic touch slider

--- public/javascripts/slider.js
+++ public/javascripts/slider.js
@@ -1,34 +1,6 @@
 $(document).ready(function() {
   // slider js
-  // aperture slider
-  $('#f_slider').slider({
-    formater: function(value) {
-      return 'f/' + value;
-    }
-  });
-  $('#f_lock').click(function() {
-    if(this.checked) {
-      var status = $('#f_slider').slider('isEnabled');
-      alert(status);
-      $('#f_slider').slider('enable');
-    }
-    else {
-      $('#f_slider').slider('disable'); 
-    }
-  });
-
-  // shutter speed slider
-  $('#s_slider').slider({
-    formater: function(value) {
-      return value + 's';
-    }
-  });
-  $('#s_lock').click(function() {
-    if(this.checked) {
-      $('#s_slider').slider('enable');
-    }
-    else {
-      $('#s_slider').slider('disable'); 
-    }
-  }); // end slider js
+  $( "#f_slider" ).slider();
+  $( "#s_slider" ).slider();
+  // end slider js
 }); // end document js
