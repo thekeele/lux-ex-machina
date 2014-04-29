d8d5f02db82344605d80c27943832e5155fc7216
Captain Underpants <mkeele@indiana.edu>
2014-04-29 19:08:11 -0400

    clean up ui, Helvetica for jay

--- public/javascripts/slider.js
+++ public/javascripts/slider.js
@@ -1,10 +1,13 @@
-$(document).ready(function() {
-  /* slider js */
+/*
+ * Lib
+ * slider.js
+ */
 
+$(document).ready(function() {
   /* Typical one-half-stop f-number scale */
   var fRange = [0.70, 0.84, 1.0, 1.2, 1.4, 1.7, 2, 2.4, 2.8, 3.3, 4, 4.8, 5.6, 6.7, 8, 9.5, 11, 13, 16, 19, 22, 27, 32];
 
-  // aperture slider
+  /* aperture slider */
   $( "#f_slider" ).slider({ 
     animate: "slow", // or "fast"
     max: fRange.length - 1,
@@ -19,7 +22,7 @@ $(document).ready(function() {
   var sRange = [1/1000, 1/500, 1/250, 1/125, 1/60, 1/30, 1/15, 1/8, 1/4, 1/2, 1];
   var sRangeStr = ['1/1000', '1/500', '1/250', '1/125', '1/60', '1/30', '1/15', '1/8', '1/4', '1/2', '1'];
 
-  // shutter speed slider
+  /* shutter speed slider */
   $( "#s_slider" ).slider({ 
     animate: "slow", // or "fast"
     max: sRange.length - 1,
@@ -28,11 +31,11 @@ $(document).ready(function() {
     slide: function(event, ui) {                    
       $("#s_amount").val(sRangeStr[ui.value] + ' s');           
     }
-  });// end slider js
+  });
 
-  // lock button js
-  $( "button#i_lock" ).click(function() {
-    if ($( "i#i_icon" ).hasClass("fa fa-unlock-alt fa-2x") && $( "#iso_select" ).prop("disabled", false)) { //disable selector
+  /* lock button js */
+  $( "button#i_lock" ).click(function() { //disable selector
+    if ($( "i#i_icon" ).hasClass("fa fa-unlock-alt fa-2x") && $( "#iso_select" ).prop("disabled", false)) {
       $( "i#i_icon" ).removeClass("fa fa-unlock-alt fa-2x");
       $( "i#i_icon" ).addClass("fa fa-lock fa-2x");
       $( "#iso_select" ).prop("disabled", "disabled");
@@ -42,8 +45,8 @@ $(document).ready(function() {
       $( "#iso_select" ).prop("disabled", false);
     }
   });
-  $( "button#f_lock" ).click(function() {
-    if ($( "i#f_icon" ).hasClass("fa fa-unlock-alt fa-2x") && $( "#f_slider" ).slider( "enable" )) { // lock the slider
+  $( "button#f_lock" ).click(function() { // lock the slider
+    if ($( "i#f_icon" ).hasClass("fa fa-unlock-alt fa-2x") && $( "#f_slider" ).slider( "enable" )) {
       $( "i#f_icon" ).removeClass("fa fa-unlock-alt fa-2x");
       $( "i#f_icon" ).addClass("fa fa-lock fa-2x");
       $( "#f_slider" ).slider( "disable" );
@@ -55,8 +58,8 @@ $(document).ready(function() {
       //$( "#f_amount" ).prop("readonly", false);
     }
   });
-  $( "button#s_lock" ).click(function() {
-    if ($( "i#s_icon" ).hasClass("fa fa-unlock-alt fa-2x") && $( "#s_slider" ).slider( "enable" )) { // lock the slider
+  $( "button#s_lock" ).click(function() { // lock the slider
+    if ($( "i#s_icon" ).hasClass("fa fa-unlock-alt fa-2x") && $( "#s_slider" ).slider( "enable" )) {
       $( "i#s_icon" ).removeClass("fa fa-unlock-alt fa-2x");
       $( "i#s_icon" ).addClass("fa fa-lock fa-2x");
       $( "#s_slider" ).slider( "disable" );
