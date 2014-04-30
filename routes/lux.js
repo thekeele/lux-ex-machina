7918aa305586a6e989711ce6e3cbbec22d8f4559
Captain Underpants <mkeele@indiana.edu>
2014-04-30 15:24:20 -0400

    should have been part of last commit :(

--- routes/lux.js
+++ routes/lux.js
@@ -9,8 +9,8 @@ var lux = require('../models/lux');
 /* GET lux page */
 exports.findAll = function(req, res) {
     
-    lux.return_json(function(result) {
-        console.log(result);
-        res.send(result);  
+    lux.return_results(function(result, computations) {
+    	result['computations'] = computations;
+        res.send(result);
     });
 };
