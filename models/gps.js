5cb09f96cc262611249680fe3b26f32be255f16c
Captain Underpants <mkeele@indiana.edu>
2014-04-30 02:33:08 -0400

    compute ev, f, s, from lux and iso

--- models/gps.js
+++ models/gps.js
@@ -24,10 +24,10 @@ var options = {
 
 /* GET JSON Object */
 rest.getJSON(options, function(result, status_code) {
-	console.log('STATUS: '+ status_code + ' JSON: ' + JSON.stringify(result));
+	//console.log('STATUS: '+ status_code + ' JSON: ' + JSON.stringify(result));
 	json_object = JSON.stringify(result);
 
-	/* export object to be used in other files */
+	// export object to be used in other files 
 	exports.return_json = function(onResult) {
 		console.log(json_object.toString());
 		onResult(json_object);
--- models/lux.js
+++ models/lux.js
@@ -4,8 +4,8 @@
  */
 
 /* Include Modules */
-var rest = require('../lib/rest');
-var json_object = '';
+var rest = require('../lib/rest')
+    , mathjs = require('mathjs');
 
 /* HTTP Options */
 var options = {
@@ -22,14 +22,75 @@ var options = {
     }
 };
 
+function compute_exposure(luminosity, iso) {
+    //exposure = logbase2( luminosity / 2.5 )
+    var ev, raw_ev;
+    luminosity = parseInt(luminosity, 10);
+    raw_ev = math.log((luminosity/2.5), 2);
+    ev = math.round(raw_ev, 0);
+
+    switch (iso) {
+        case 200:
+            return ev - 1;
+        case 400:
+            return ev - 2;
+        case 800:
+            return ev - 3;
+        case 1600:
+            return ev - 4;
+        case 3200:
+            return ev - 5;
+        default:
+            return ev;
+    }
+}
+
+function compute_aperture(exposure, shutter) {
+    //aperture = sqrt( 2^EV * shutter )
+    console.log("f: " + exposure);
+    return math.sqrt(math.pow(2, exposure) * shutter);
+}
+
+function compute_shutter(exposure, aperture) {
+    //shutter = ( aperture^2 ) / ( 2^EV )
+    console.log("s: " + exposure);
+    return ( math.pow(aperture, 2) / math.pow(2, exposure) );
+}
+
 /* GET JSON Object */
 rest.getJSON(options, function(result, status_code) {
-	console.log('STATUS: '+ status_code + ' JSON: ' + JSON.stringify(result));
-	json_object = JSON.stringify(result);
+    var i;
+    var ev;
+    var ap;
+    var ss;
+    var iso = 100;
+    var aperture = 2;
+    var lum = result.luminosity[0].value;
+    var latest = result.luminosity[0].timestamp;
+    math = mathjs();
 
-	/* export object to be used in other files */
+    for (i=0; i < result.luminosity.length; i++) {
+        if (result.luminosity[i].timestamp > latest) {
+            latest = result.luminosity[i].timestamp;
+            lum = result.luminosity[i].value;
+        }
+    }
+
+    lum = 80;
+    ev = compute_exposure(lum, iso);
+    ss = compute_shutter(ev, aperture); //initial guess for aperture
+    ap = compute_aperture(ev, ss);
+    ss = compute_shutter(ev, ap);
+
+    console.log(result);
+    console.log("lumens: " + lum);
+    console.log("iso: " + iso);
+    console.log("exposure value: " + ev);
+    console.log("aperture: " + ap);
+    console.log("shutter speed: " + ss);
+
+	// export object to be used in other files 
 	exports.return_json = function(onResult) {
-		console.log(json_object.toString());
-		onResult(json_object);
+		onResult(lum);
 	}
 });
