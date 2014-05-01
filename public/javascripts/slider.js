5aeb11756962f37512541f1ee3cfe93aab72eacf
Captain Underpants <mkeele@indiana.edu>
2014-05-01 18:42:47 -0400

    aperture/shutter priority modes, show error if exist

--- public/javascripts/slider.js
+++ public/javascripts/slider.js
@@ -4,11 +4,17 @@
  */
 
 $(document).ready(function() {
-  /* Typical one-half-stop f-number scale */
-  var fRange = [0.70, 0.84, 1.0, 1.2, 1.4, 1.7, 2, 2.4, 2.8, 3.3, 4, 4.8, 5.6, 6.7, 8, 9.5, 11, 13, 16, 19, 22, 27, 32];
+  /* Typical one-half-stop f-number scale(wikipedia) */
+  var sRange = [960, 480, 240, 120, 60, 30, 15, 8, 4, 2, 1, 1/2, 1/4, 1/8, 1/15, 1/30, 1/60, 1/125, 1/250, 1/500, 1/1000, 1/2000, 1/4000, 1/8000, 1/15000, 1/30000, 1/60000];
+  var sRangeStr = ['960', '480', '240', '120', '60', '30', '15', '8', '4', '2', '1', '1/2', '1/4', '1/8', '1/15', '1/30', '1/60', '1/125', '1/250', '1/500', '1/1000', '1/2000', '1/4000', '1/8000', '1/15000', '1/30000', '1/60000'];
+  var fRange = [1.4, 2, 2.8, 4, 5.6, 8, 11, 16, 22, 32];
   var aperture = $('#f_amount').data('aperture'); 
   var shutter = $('#s_amount').data('shutter');
   var iso = $('#iso_select').data('iso');
+  var fPosition;
+  var sPosition;
+  var fChange = false;
+  var sChange = false;
 
   /* iso value */
   $('#iso_select').val(iso);
@@ -19,29 +25,98 @@ $(document).ready(function() {
     max: fRange.length - 1,
     min: 0,
     create: function(event, ui) {
-      var position = fRange.indexOf(aperture);
-      $('#f_slider').slider("option", "value", position);
+      fPosition = fRange.indexOf(aperture);
+      $('#f_slider').slider("option", "value", fPosition);
     },
     slide: function(event, ui) {                        
       $("#f_amount").val('f/' + fRange[ui.value]);
-    } 
+    },
+    change: function(event, ui) {
+      if(sChange) {
+        // do nothing
+        sChange = false;
+      } else {
+        fChange = true;
+        var value = $("#f_slider").slider("option", "value");
+
+        if(fPosition < value) 
+        { //increasing aperture value
+          var shutter = $("#s_slider").slider("option", "value");
+          shutter = shutter - (value - fPosition);
+          $("#s_slider").slider("option", "value", shutter);
+          $("#s_amount").val(sRangeStr[shutter] + ' s');
+        }
+        else if(fPosition > value) 
+        { //decreasing aperture value
+          var shutter = $("#s_slider").slider("option", "value");
+          shutter = shutter + (fPosition - value);
+          $("#s_slider").slider("option", "value", shutter);
+          $("#s_amount").val(sRangeStr[shutter] + ' s');
+        } 
+        else 
+        {
+          console.log("equal");
+        }
+
+        fPosition = value;
+      }
+    }
   });
-
-  /* agreed standards for shutter speeds */
-  var sRange = [1/1000, 1/500, 1/250, 1/125, 1/60, 1/32, 1/15, 1/8, 1/4, 1/2, 1];
-  var sRangeStr = ['1/1000', '1/500', '1/250', '1/125', '1/60', '1/32', '1/15', '1/8', '1/4', '1/2', '1'];
-
+  
   /* shutter speed slider */
   $( "#s_slider" ).slider({ 
     animate: "slow", // or "fast"
     max: sRange.length - 1,
     min: 0,
     create: function(event, ui) {
-      var position = sRange.indexOf(shutter);
-      $('#s_slider').slider("option", "value", position);
+      sPosition = sRange.indexOf(shutter);
+      $('#s_slider').slider("option", "value", sPosition);
     },
     slide: function(event, ui) {                    
       $("#s_amount").val(sRangeStr[ui.value] + ' s');           
+    },
+    change: function(event, ui) {
+      if(fChange) {
+        //do nothing
+        fChange = false;
+      } else {
+        sChange = true;
+        var value = $("#s_slider").slider("option", "value");
+
+        if(sPosition < value) 
+        { //decreasing shutter value
+          var aperture = $("#f_slider").slider("option", "value");
+          aperture = aperture + (sPosition - value);
+
+          if(fRange[aperture] === undefined) {
+            $('#main_content').css('background-color', '#ff0000');
+          } else {
+            $('#main_content').css('background-color', '#ffffff');
+          }
+
+          $("#f_slider").slider("option", "value", aperture);
+          $("#f_amount").val('f/' + fRange[aperture]);
+        }
+        else if(sPosition > value) 
+        { //decreasing shutter value
+          var aperture = $("#f_slider").slider("option", "value");
+          aperture = aperture - (value - sPosition);
+
+          if(fRange[aperture] === undefined) {
+            $('#main_content').css('background-color', '#ff0000');
+          } else {
+            $('#main_content').css('background-color', '#ffffff');
+          }
+
+          $("#f_slider").slider("option", "value", aperture);
+          $("#f_amount").val('f/' + fRange[aperture]);
+        } 
+        else 
+        {
+          console.log("equal");
+        }
+        sPosition = value;
+      }
     }
   });
 
@@ -84,3 +159,8 @@ $(document).ready(function() {
     }
   });//end lock button js
 }); // end document js
+
+//var fRange = [0.70, 0.84, 1.0, 1.2, 1.4, 1.7, 2, 2.4, 2.8, 3.3, 4, 4.8, 5.6, 6.7, 8, 9.5, 11, 13, 16, 19, 22, 27, 32];
+/* agreed standards for shutter speeds(wikipedia) */
+//var sRange = [1/1000, 1/500, 1/250, 1/125, 1/60, 1/32, 1/15, 1/8, 1/4, 1/2, 1];
+//var sRangeStr = ['1/1000', '1/500', '1/250', '1/125', '1/60', '1/32', '1/15', '1/8', '1/4', '1/2', '1'];
